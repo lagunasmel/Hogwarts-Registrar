ca4f80d96a0e15e2cb9618306c875b53e39ec962
Melissa Lagunas <lagunasmel@gmail.com>
2020-10-31 14:01:07 -0400

    Revert "Update app.py"
    This reverts commit 864b0f0efecbb8356bd03463ee62ac257a9d0c8f.

--- app.py
+++ app.py
@@ -10,6 +10,18 @@ def hello_world():
 
 @hogwarts.route('/')
 def index():
+    posts = [
+        {'title': 'First Post',
+         'author': 'Mel',
+         'date': 'April 20',
+         'content': 'First Blog post'
+         },
+        {'title': '2nd Post',
+         'author': 'Mel',
+         'date': 'April 26',
+         'content': '2nd Blog post'
+         }
+    ]
     return render_template('index.html', posts=posts)
 
 
